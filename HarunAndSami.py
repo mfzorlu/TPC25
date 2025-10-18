ea1d42144045aec9bb8d768641fe6b715f169b70
metn-u <metincan.avar21@gmail.com>
2025-10-18 22:07:05 +0300

    Create HarunAndSami.py

---
+++ HarunAndSami.py
@@ -0,0 +1,22 @@
+liste = input()
+a,b = map(int,liste.split())
+
+c = input()
+
+harun = 0
+sami = 0
+
+
+
+for i in c:
+    if i == "H":
+        harun +=1
+    else:
+        sami +=1
+
+if harun > a/2:
+    print("Harun")
+elif sami > a/2:
+    print("sami")
+else:
+    print("Cilek")
