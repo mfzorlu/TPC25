b68b849ef6c0a6761b89e20524e757cfe5c761bf
mfzorlu <muhammetfurkanzorlu@ogr.iu.edu.tr>
2025-10-18 22:33:18 +0300

    Create usain bolt.py

---
+++ usain bolt.py
@@ -0,0 +1,17 @@
+# Practice makes it perfect
+# inzva community built algoleague for every algorithm enthusiast hungry 
+# for self-improvement and friendly competition. Have fun and good luck!
+
+n = int(input())
+array = str(input())
+array = array.split()
+
+result = 0
+c = array[0]
+
+for i in range(1,n):
+    if int(array[i]) > c:
+        c = int(array[i])
+        result += 1
+
+print(result)
