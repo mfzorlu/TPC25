18b50004744701db37bbf84922ed7cc990e4c702
mfzorlu <muhammetfurkanzorlu@ogr.iu.edu.tr>
2025-10-18 23:42:28 +0300

    Update MishaAndCookies.py

--- MishaAndCookies.py
+++ MishaAndCookies.py
@@ -4,32 +4,28 @@ array = input().split()
 
 array = list(map(int,array))
 
-c = []
-t = []
+c = 0
+t=0
+sc = 0
+st = 0
+
 
 for i in range(n):
     if int(array[i]) % 2 == 0:
-        c.append(i)
+        c += 1
+        sc += i
     else:
-        t.append(i)
-
-
-sc = 0
-for i in c:
-    sc += i
-st = 0
-for i in t:
-    st += i
-
+        t+=1
+        st += i
 
 
 neg = 0
-if sc/len(c) < (n-1)/2:
-    for i in range(len(c)):
+if sc/c < (n-1)/2:
+    for i in range(c):
         neg += i
     print(sc-neg)
 
 else:
-    for i in range(len(t)):
+    for i in range(t):
         neg += i
     print(st-neg)
