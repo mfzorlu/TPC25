44e59ca508fb904605e262917b28588efab0d77e
Muhammet Furkan Zorlu <muhammetfurkanzorlu@ogr.iu.edu.tr>
2025-12-06 10:34:58 +0300

    Update soru.py
    input

---
+++ Kutluhan:gölgeler diyarı/soru.py
@@ -0,0 +1,14 @@
+t = int(input())
+N, D, Ai, Bi, Ci = []
+for i in range(t):
+  liste = input()
+  xN, xD = map(int,liste.split())
+  N.append(xN)
+  D.append(xD)
+  for i in range(N):
+    liste = input()
+    xAi, xBi, xCi = map(int,liste.split())
+    Ai.append(xAi)
+    Bi.append(xBi)
+    Ci.append(xCi)
+
