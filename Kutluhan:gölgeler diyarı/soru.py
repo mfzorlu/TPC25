d56957e173523a5cae441c81e1dd73ca5a8b80a1
metn-u <metincan.avar21@gmail.com>
2025-12-06 12:11:44 +0300

    fonksiyon kullanılmyor

--- Kutluhan:gölgeler diyarı/soru.py
+++ Kutluhan:gölgeler diyarı/soru.py
@@ -1,47 +1,44 @@
-def solve():
-    t = int(input())
+t = int(input())
 
-    for _ in range(t):
-        N, D = map(int, input().split())
+for _ in range(t):
+    N, D = map(int, input().split())
 
-        canavarlar = []
-        toplam_hasar = 0
-        mumkun = True
+    canavarlar = []
+    toplam_hasar = 0
+    mumkun = True
 
-        for _ in range(N):
-            A, B, C = map(int, input().split())
-            if not mumkun:
+    for _ in range(N):
+        A, B, C = map(int, input().split())
+        if not mumkun:
+            continue
+
+        toplam_hasar += B
+        tur = 0
+
+        if A <= D:
+            tur = 1
+        else:
+            if C >= D:
+                mumkun = False
                 continue
 
-            toplam_hasar += B
-            tur = 0
+            kalan_can = A - D
+            net_hasar = D - C
+            tur = 1 + (kalan_can + net_hasar - 1) // net_hasar
 
-            if A <= D:
-                tur = 1
-            else:
-                if C >= D:
-                    mumkun = False
-                    continue
+        canavarlar.append((tur, B))
 
-                kalan_can = A - D
-                net_hasar = D - C
-                tur = 1 + (kalan_can + net_hasar - 1) // net_hasar
+    if not mumkun:
+        print("-1")
+    else:
+        canavarlar.sort(key=lambda x: x[0] / x[1])
 
-            canavarlar.append((tur, B))
+        toplam_yedigimiz_hasar = 0
+        su_anki_toplam_hasar = toplam_hasar
 
-        if not mumkun:
-            print("-1")
-        else:
-            canavarlar.sort(key=lambda x: x[0] / x[1])
+        for tur, hasar in canavarlar:
+            digerlerinin_hasari = su_anki_toplam_hasar - hasar
+            toplam_yedigimiz_hasar += digerlerinin_hasari * tur
+            su_anki_toplam_hasar -= hasar
 
-            toplam_yedigimiz_hasar = 0
-            su_anki_toplam_hasar = toplam_hasar
-
-            for tur, hasar in canavarlar:
-                digerlerinin_hasari = su_anki_toplam_hasar - hasar
-                toplam_yedigimiz_hasar += digerlerinin_hasari * tur
-                su_anki_toplam_hasar -= hasar
-
-            print(toplam_yedigimiz_hasar)
-
-solve()
+        print(toplam_yedigimiz_hasar)
