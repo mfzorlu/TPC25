5af06c1e7bbbb0f7c66d6ee8f3f73b240552203e
Muhammet Furkan Zorlu <muhammetfurkanzorlu@ogr.iu.edu.tr>
2025-12-06 10:58:12 +0300

    Create soru.c

---
+++ soru.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+  int t;
+  int N[];
+  int D[];
+  int Ai[][];
+  int Bi[][];
+  int Ci[][];
+
+  scanf("%d", &t);
+  int i;
+  for (i=0;i<t;i++) {
+    
+  }
+
+
+
+
+
+
+  return 0;
+}
